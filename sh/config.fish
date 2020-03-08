1c27ee7087a3eed5e24fc9bfc52236a375a6eaee
phynalle <phynalism@gmail.com>
2020-03-09 02:50:05 +0900

    Set theme_color_theme for dracula

--- sh/config.fish
+++ sh/config.fish
@@ -10,3 +10,6 @@ alias vi=nvim
 alias vim=nvim
 alias viconf="vi ~/.config/nvim/init.vim"
 alias conf="vi $CONFIG_PATH; and source $CONFIG_PATH"
+
+# Display
+set -g theme_color_scheme terminal2-dark
