fe5f2722e1ac61e3bea9642b3f971940be3828c5
phynalle <phynalism@gmail.com>
2024-02-13 16:01:48 +0900

    Add wezterm config

---
+++ sh/.wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+config.default_prog = { 'nu', '-l' }
+
+config.color_scheme = 'Gruvbox Material (Gogh)'
+config.font = wezterm.font 'Cascadia Code PL'
+config.font_size = 10.0
+
+config.keys = {
+  { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right' },
+}
+
+return config
