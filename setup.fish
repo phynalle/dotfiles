b5a2857e6af98d44ab59793ce42a4acb232e4bcb
phynalle <phynalism@gmail.com>
2024-02-13 15:15:20 +0900

    Remove vim settings

--- setup.fish
+++ setup.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
 set -x FISH_CONFIG_PATH ~/.config/fish/config.fish
-set -x VIM_CONFIG_PATH ~/.config/nvim/init.vim
 
 curl -L https://get.oh-my.fish | fish
 omf install fzf
@@ -10,13 +9,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 mkdir -p (dirname $FISH_CONFIG_PATH)
 ln -s (realpath sh/config.fish) $FISH_CONFIG_PATH
 source $FISH_CONFIG_PATH
-
-mkdir -p (dirname $VIM_CONFIG_PATH)
-ln -s (realpath vim/base.vim) $VIM_CONFIG_PATH
-
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vi +PlugInstall
 
 # Settings for rust
 rustup install stable
