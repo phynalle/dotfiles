794c0dc3a3f2990e42982d8b4ca5ec71f8bfb3be
phynalle <phynalism@gmail.com>
2024-02-13 19:42:57 +0900

    No padding on WezTerm

--- configs/wezterm/config.lua
+++ configs/wezterm/config.lua
@@ -7,6 +7,12 @@ config.default_prog = { 'nu', '-l' }
 config.color_scheme = 'Gruvbox Material (Gogh)'
 config.font = wezterm.font 'Cascadia Code PL'
 config.font_size = 10.0
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 config.keys = {
   { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left' },
