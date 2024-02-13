86986debdb4bd8fd5e345d66d13f6339fc1302ec
phynalle <phynalism@gmail.com>
2024-02-13 20:50:38 +0900

    Change config.fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,17 +1,17 @@
 set -x CONFIG_PATH "~/.config/fish/config.fish"
 
-set -x DENO_INSTALL "/home/phynalle/.local"
+set -x DENO_INSTALL "~/.local"
 set -a PATH "$DENO_INSTALL/bin"
 
 source ~/.cargo/env
-set -xg EDITOR vim
+set -xg EDITOR hx
 
-direnv hook fish | source
+eval (direnv hook fish)
+jj util completion fish | source
 
 alias ls=exa
 alias vi=nvim
 alias vim=nvim
-alias viconf="vi ~/.config/nvim/init.vim"
 alias conf="vi $CONFIG_PATH; and source $CONFIG_PATH"
 alias kz="date '+%Y-%0m-%d %H:%M:%S %z (%s)'"
 
